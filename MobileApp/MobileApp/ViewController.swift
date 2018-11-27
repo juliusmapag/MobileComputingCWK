ffaf2f5f0503b4646bd509f1152ecd5ee5baafb7
juliusmapag <44968995+juliusmapag@users.noreply.github.com>
2018-11-27 19:51:06 +0000

    Add files via upload
    Coursework v3. Up to step 5a.

--- MobileApp/MobileApp/ViewController.swift
+++ MobileApp/MobileApp/ViewController.swift
@@ -14,9 +14,15 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var cloud: UIImageView!
     
+    @IBOutlet weak var cloud1: UIImageView!
+    
     @IBOutlet weak var road: UIImageView!
     
     @IBOutlet weak var tree: UIImageView!
+    
+    @IBOutlet weak var crows: UIImageView!
+    
+    @IBOutlet weak var crows1: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,13 +46,20 @@ class ViewController: UIViewController {
                       UIImage(named: "plane15.png")!]
         
         plane.image = UIImage.animatedImage(with: planeArray, duration: 1)
-            
+    
         var cloudArray: [UIImage]!
             
         cloudArray = [UIImage(named: "cloud.png")!,
                       UIImage(named: "cloud2.png")!]
 
         cloud.image = UIImage.animatedImage(with: cloudArray, duration: 1)
+        
+        var cloud1Array: [UIImage]!
+        
+        cloud1Array = [UIImage(named: "cloud.png")!,
+                      UIImage(named: "cloud2.png")!]
+        
+        cloud1.image = UIImage.animatedImage(with: cloud1Array, duration: 1)
         
         var roadArray: [UIImage]!
         
@@ -83,9 +96,66 @@ class ViewController: UIViewController {
                      UIImage(named: "tree7.png")!,
                      UIImage(named: "tree8.png")!,
                      UIImage(named: "tree9.png")!,
-                     UIImage(named: "tree10.png")!]
+                     UIImage(named: "tree10.png")!,
+                     UIImage(named: "tree11.png")!,
+                     UIImage(named: "tree12.png")!,
+                     UIImage(named: "tree13.png")!,
+                     UIImage(named: "tree14.png")!,
+                     UIImage(named: "tree15.png")!,
+                     UIImage(named: "tree16.png")!,
+                     UIImage(named: "tree17.png")!]
+        
         
         tree.image = UIImage.animatedImage(with: treeArray, duration: 1)
+        
+        var crowsArray: [UIImage]!
+        
+        crowsArray = [UIImage(named: "bird1.png")!,
+                     UIImage(named:  "bird2.png")!,
+                     UIImage(named:  "bird3.png")!,
+                     UIImage(named:  "bird4.png")!,
+                     UIImage(named:  "bird5.png")!,
+                     UIImage(named:  "bird6.png")!,
+                     UIImage(named:  "bird7.png")!,
+                     UIImage(named:  "bird8.png")!,
+                     UIImage(named:  "bird9.png")!,
+                     UIImage(named:  "bird10.png")!]
+        
+        crows.image = UIImage.animatedImage(with: crowsArray, duration: 1)
+        
+        var crows1Array: [UIImage]!
+        
+        crows1Array = [UIImage(named: "bird1.png")!,
+                      UIImage(named:  "bird2.png")!,
+                      UIImage(named:  "bird3.png")!,
+                      UIImage(named:  "bird4.png")!,
+                      UIImage(named:  "bird5.png")!,
+                      UIImage(named:  "bird6.png")!,
+                      UIImage(named:  "bird7.png")!,
+                      UIImage(named:  "bird8.png")!,
+                      UIImage(named:  "bird9.png")!,
+                      UIImage(named:  "bird10.png")!]
+        
+        crows.image = UIImage.animatedImage(with: crowsArray, duration: 1)
+        
+        UIView.animate(withDuration: 5.0, delay: 1.0, options: [UIView.AnimationOptions.repeat, .curveLinear],
+                      animations:
+                      {
+                            self.crows.center.x -= self.view.bounds.width
+                
+        }, completion: nil
+        )
+        
+        crows1.image = UIImage.animatedImage(with: crows1Array, duration: 1)
+        
+        UIView.animate(withDuration: 4.0, delay: 2.0, options: [UIView.AnimationOptions.repeat, .curveLinear],
+                       animations:
+            {
+                self.crows1.center.x -= self.view.bounds.width
+                
+        }, completion: nil
+        )
+
 
             
     }
